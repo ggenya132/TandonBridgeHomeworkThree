2c1914d89e01233d95c1604ca49b1caa243e5c71
ggenya132 <eugene.vedensky@gmail.com>
2024-09-23 22:48:34 -0400

    Adding question six

--- ev2361_hw3_q6.cpp
+++ ev2361_hw3_q6.cpp
@@ -1,44 +1,91 @@
 #include <iostream>
+
+#include <map>
+
+#include <string>
+
 using namespace std;
 
-int main()
-{
-    double priceFirstItem, priceSecondItem, taxRate, priceDiscount;
-    char customerHasClubCardAnswer;
-    bool hasClubCard;
+void endl() {
+  cout << endl;
+}
+class CallDetails {
+  public: double startingHour;
+  double startingMinute;
+  double endingHour;
+  double endingMinute;
+  string dayOfCall;
+};
 
-    cout << "Enter	price	of	first	item: ";
-    cin >> priceFirstItem;
-    cout << endl;
-    cout << "Enter	price	of	second	item: ";
-    cin >> priceSecondItem;
-    cout << endl;
-    cout << "Does	customer have a	club card? (Y/N): ";
-    cin >> customerHasClubCardAnswer;
-    cout << endl;
-    cout << "Enter	tax	rate, e.g. 5.5	for	5.5% tax: ";
-    cin >> taxRate;
-    cout << endl;
-
-    hasClubCard = customerHasClubCardAnswer == 'y' ? true : false;
-    priceDiscount = hasClubCard ? .9 : 1;
-
-    double totalBasePrice = priceFirstItem + priceSecondItem;
-
-    if (priceFirstItem <= priceSecondItem)
-    {
-        priceFirstItem = priceFirstItem * .5;
+double getRate(string dayOfcall, double hour) {
+    if (dayOfcall == "Sa" || dayOfcall == "Su") {
+        return 0.15; // Weekend rate
     }
-    else
-    {
-        priceSecondItem = priceSecondItem * .5;
+    // Weekday rates
+    if (hour >= 8 && hour < 18) {
+        return 0.40; // Daytime rate
     }
-    double totalPriceWithDiscounts = (priceFirstItem + priceSecondItem) * priceDiscount;
-    double priceWithTax = totalPriceWithDiscounts * (1 + taxRate);
+    return 0.25; // Evening/Night rate
+}
+double calculateCost(CallDetails callDetails){
 
-    cout << "Base price: " << totalBasePrice << endl;
-    cout << "Price after discounts: " << totalPriceWithDiscounts << endl;
-    cout << "Total price: " << priceWithTax << endl;
+    double currentHour = callDetails.startingHour;
+    double endingHour = callDetails.endingHour;
 
-    return 0;
+    const double rate = getRate(callDetails.dayOfCall, callDetails.startingHour);
+
+    if(currentHour == endingHour){
+        return rate * (callDetails.endingMinute - callDetails.startingMinute);
+    }
+
+    double totalCost = 0; 
+
+
+    double costOfFirstHourReducedForMinutesNotSpent = rate * (60 - callDetails.startingMinute);
+
+    totalCost += costOfFirstHourReducedForMinutesNotSpent;
+
+    while(currentHour+1 <= endingHour - 1){
+        totalCost += rate * 60;
+        currentHour++;
+    } 
+    double costOfLastHour = callDetails.endingMinute * rate;
+
+    totalCost += costOfLastHour;
+    return totalCost;
+
+};
+
+int main() {
+
+  double startingHour, startingMinute, endingHour, endingMinute;
+  string dayOfPhoneCall;
+
+  cout << "Please enter the day of the phone call in the following format, Mo, Tu, We, Th, Fr, Sa or Su: ";
+  cin >> dayOfPhoneCall;
+  endl();
+  cout << "Please enter the starting hour of phone call in 24 hour format (00-24): ";
+  cin >> startingHour;
+  endl();
+  cout << "Please enter the starting minute of phone call (0-59): ";
+  cin >> startingMinute;
+  endl();
+  cout << "Please ending hour of phone call in 24 hour format (00-24): ";
+  cin >> endingHour;
+  endl();
+  cout << "Please enter ending minute of phone call (0-59): ";
+  cin >> endingMinute;
+  endl();
+
+  CallDetails callDetail;
+  callDetail.dayOfCall = dayOfPhoneCall;
+  callDetail.startingHour = startingHour;
+  callDetail.startingMinute = startingMinute;
+  callDetail.endingHour = endingHour;
+  callDetail.endingMinute = endingMinute;
+
+  cout<<"Total cost is: $"<<calculateCost(callDetail);
+
+  return 1;
+
 }
